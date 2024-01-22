8f16d700d0eea0129bf5b41dc10752032bec9e18
lodmev <lodmev@gmail.com>
2024-01-22 23:35:05 +0300

    feat: solve 22 tasks (36  passing)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -371,8 +371,13 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let prevElement = -1;
+  const result = Array.from({ length: len });
+  return result.map(() => {
+    prevElement += 2;
+    return prevElement;
+  });
 }
 
 /**
